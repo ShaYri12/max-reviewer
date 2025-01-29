b3b68cfea9da87e7773ac441cb838197c5e7f726
vikram kumar <vikramkumar8t8@gmail.com>
2025-01-30 03:43:28 +0500

    fixed

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -208,7 +208,7 @@ const SignupForm = ({ userData, onSubmit }) => {
         </div>
       )}
 
-      <div className=" bg-[#17375F] flex items-center justify-center px-4 overflow-hidden">
+      <div className=" bg-[#17375F] flex items-center justify-center px-4 overflow-hidden fixed">
         <div className="w-full max-w-md h-full mt-16 md:mt-0">
           <div className="flex flex-col items-center md:mb-8 mb-4">
             <div className="flex items-center gap-2 md:mb-3 px-6 pt-6 md:pb-6 pb-4">
