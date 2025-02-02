2ab803748d41fb45c2ed0f274fe10d126ca79252
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-02-02 15:24:38 +0500

    testing

--- src/app/interface/page.js
+++ src/app/interface/page.js
@@ -71,56 +71,60 @@ const Interface = () => {
   };
 
   return (
-    <div className="h-dvh bg-[#17375F] overflow-y-hidden">
-      <Navbar />
+    <div className="relative">
+      <div className="h-dvh bg-[#17375F] overflow-y-hidden">
+        <Navbar />
 
-      <div className="bg-white top-[80px] fixed max-w-md mx-auto bottom-0 right-4 left-4 rounded-t-xl">
-        <main className="px-4 py-6 h-full">
-          <div className="overflow-y-auto h-full flex flex-col justify-between">
-            <div className="h-full">
-              <div className="flex items-center justify-between mb-4">
-                <div className="flex items-center gap-2">
-                  <img
-                    src="/qr-code.svg"
-                    className="w-[20px] h-[20px] object-contain overflow-hidden"
-                  />
-                  <h2 className="text-lg font-semibold text-[#6C7278]">
-                    Tus productos ({products.length})
-                  </h2>
+        <div className="bg-white top-[80px] fixed max-w-md mx-auto bottom-0 right-4 left-4 rounded-t-xl">
+          <main className="px-4 py-6 h-full">
+            <div className="overflow-y-auto h-full flex flex-col justify-between">
+              <div className="h-full">
+                <div className="flex items-center justify-between mb-4">
+                  <div className="flex items-center gap-2">
+                    <img
+                      src="/qr-code.svg"
+                      className="w-[20px] h-[20px] object-contain overflow-hidden"
+                    />
+                    <h2 className="text-lg font-semibold text-[#6C7278]">
+                      Tus productos ({products.length})
+                    </h2>
+                  </div>
+                  <button
+                    onClick={() => router.push("/add-product")}
+                    className="text-[#6DC1E6]"
+                  >
+                    <img
+                      src="/plus.svg"
+                      className="w-[30px] h-[30px] object-contain overflow-hidden"
+                    />
+                  </button>
                 </div>
-                <button
-                  onClick={() => router.push("/add-product")}
-                  className="text-[#6DC1E6]"
-                >
-                  <img
-                    src="/plus.svg"
-                    className="w-[30px] h-[30px] object-contain overflow-hidden"
-                  />
-                </button>
-              </div>
 
-              {loading ? (
-                <p className="text-center text-gray-600">Loading...</p>
-              ) : products.length === 0 ? (
-                <p className="text-center text-gray-600">No products found.</p>
-              ) : (
-                products.map((product) => (
-                  <ProductBox
-                    key={product.cardId}
-                    product={product}
-                    handleReviews={handleReviews}
-                    handleDelete={handleDelete}
-                    handleEditProduct={handleEditProduct}
-                  />
-                ))
-              )}
-              <div className="h-[25px]"></div>
+                {loading ? (
+                  <p className="text-center text-gray-600">Loading...</p>
+                ) : products.length === 0 ? (
+                  <p className="text-center text-gray-600">
+                    No products found.
+                  </p>
+                ) : (
+                  products.map((product) => (
+                    <ProductBox
+                      key={product.cardId}
+                      product={product}
+                      handleReviews={handleReviews}
+                      handleDelete={handleDelete}
+                      handleEditProduct={handleEditProduct}
+                    />
+                  ))
+                )}
+                <div className="h-[25px]"></div>
+              </div>
             </div>
-          </div>
-          <footer className="absolute bottom-3 left-0 w-full text-center bg-white px-4 pt-2">
-            <Footer />
-          </footer>
-        </main>
+            <footer className="absolute bottom-3 left-0 w-full text-center bg-white px-4 pt-2">
+              <Footer />
+            </footer>
+          </main>
+        </div>
       </div>
     </div>
   );
